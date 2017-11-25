f6a43d8a7a56f46553d86ae584893986fc064322
Wei Zhang <macctown@hotmail.com>
2017-11-24 23:11:10 -0500

    Change domain name

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ config = {
     // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
-        url: 'http://zhang-wei.me',
+        url: 'http://i.zhang-wei.me',
         mail: {},
         database: {
             client: 'sqlite3',
